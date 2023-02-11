2544618d97e966a48bbb477870ae0ddbb73b59be
amiralc <amira.abdrabou@esprit.tn>
2023-02-11 20:25:53 +0100

    first commit

---
+++ src/Entity/Evenement.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EvenementRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EvenementRepository::class)]
+class Evenement
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $num = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNum(): ?int
+    {
+        return $this->num;
+    }
+
+    public function setNum(?int $num): self
+    {
+        $this->num = $num;
+
+        return $this;
+    }
+}
